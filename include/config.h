e9a1fc756834e1453d43b99106409f06bfd16c56
starsoc-alex <starstar_gfgf@163.com>
2015-03-26 10:40:31 +0800

    delete all file of u-boot-2013

--- include/config.h
+++
@@ -1,6 +0,0 @@
-/* Automatically generated - do not edit */
-#define CONFIG_BOARDDIR board/xilinx/zynq_common
-#include <config_cmd_defaults.h>
-#include <config_defaults.h>
-#include <configs/star_zynq7000.h>
-#include <asm/config.h>
--- include/config.mk
+++
@@ -1,5 +0,0 @@
-ARCH   = arm
-CPU    = armv7
-BOARD  = zynq_common
-VENDOR = xilinx
-SOC    = zynq
